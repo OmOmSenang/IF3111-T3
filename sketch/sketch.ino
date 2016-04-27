1bbb247d37e3173d096f9f871736acc431e79360
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-04-27 18:03:47 +0700

    serial commands

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,21 +7,28 @@ bool turnon = true;
 
 #define fanPwmPin 11
 
+#define celsiusRange (celsiusMax-celsiusMin)
+int celsiusMin;
+int celsiusMax;
+
+#define CMDLENGTH 6 //lihat parser di bawah
+
 void setup()
 {
+celsiusMin = 17;
+celsiusMax = 34;
 Serial.begin(9600);
 pinMode(switchButtonPin, INPUT);
 attachInterrupt(digitalPinToInterrupt(switchButtonPin), switchTurnOnOffButton, RISING);  
 pinMode(fanPwmPin ,OUTPUT);
 }
 
-#define celsiusRange (celsiusMax-celsiusMin)
-#define celsiusMin 17
-#define celsiusMax 34
-
 //main loop
 void loop()
 {
+  if (Serial.available() >= CMDLENGTH){
+    handleSerialCommand();
+  }
   if (turnon){
     float humidity = getAverageHumidity(100);
     delay(100);
@@ -40,7 +47,15 @@ void loop()
     Serial.print(humidity);
     Serial.print(" % RH, vKipas: ");
     
-    Serial.println(vKipas);
+    Serial.print(vKipas*100/255);
+    Serial.println("%");
+    
+    Serial.println("");
+    Serial.println("vKipas based on temp, linear with maxtemp and mintemp, with humidity factor");
+    Serial.print("maxtemp: ");Serial.print(celsiusMax);
+    Serial.print(";mintemp: ");Serial.println(celsiusMin);
+    Serial.println("config: TMAX XX or TMIN XX, XX number");
+    Serial.println(" ");
     delay(1000);
   }else{
     analogWrite(fanPwmPin,0);
@@ -150,3 +165,38 @@ void switchTurnOnOffButton(){
   lastSignalTime=signalTime;
 }
 
+void handleSerialCommand(){
+  if (Serial.read()=='T'){
+    if (Serial.read()=='M'){
+      char a = Serial.read();
+      if (a =='A'){
+        if (Serial.read()=='X'){
+          if ( Serial.read()==' '){
+            char firstdigit = Serial.read();
+            if (firstdigit>='0'&&firstdigit<='9'){
+              char secondDigit = Serial.read();
+              if (secondDigit>='0'&&secondDigit<='9'){
+                celsiusMax = (firstdigit-'0')*10+(secondDigit-'0');
+              }
+            }
+            
+          }
+        }
+      }else if (a == 'I'){
+        if (Serial.read()=='N'){
+
+          if ( Serial.read()==' '){
+            char firstdigit = Serial.read();
+            if (firstdigit>='0'&&firstdigit<='9'){
+              char secondDigit = Serial.read();
+              if (secondDigit>='0'&&secondDigit<='9'){
+                celsiusMin = (firstdigit-'0')*10+(secondDigit-'0');
+              }
+            }
+            
+          }
+        }
+      }
+    }
+  }
+}
